e859c314a367a4c0305777b7d7ffc7825465b16e
Lia Skalkos <hello@liaskalkos.net>
2015-07-02 18:34:27 -0400

    Slight refactor

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -8,9 +8,9 @@ def even_fibonacci_sum(limit)
 
   while fib < limit
 
-    next_fib = fib #2, #3, #5, #1, #1, #2
-    fib = fib + prev_fib #2 + 1 = 3, #3 + 2 = 5, #5+3=8, #1+0=1, #1+1=2,
-    prev_fib = next_fib #2, #3, #5, #1, #1
+    next_fib = fib
+    fib = fib + prev_fib
+    prev_fib = next_fib
 
     if fib < limit
       sum+=fib if fib % 2 == 0
